a1daceabe127692f437643b388ffc55227b7b558
Tormak9970 <63308171+Tormak9970@users.noreply.github.com>
2022-05-06 22:20:29 -0500

    layout nearly finished

---
+++ src/lib/controls/ControlEntry.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    export let handler:Function;
+
+    function click(e:Event) { handler(e); }
+</script>
+
+<div class="control" on:click="{click}">
+    <slot />
+</div>
+
+<style>
+    @import '../../themes.css';
+
+    .control {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 50px;
+        border: 1px solid var(--highlight);
+
+        width: 60px;
+        height: 18px;
+        padding: 4px;
+
+        margin: 0px 5px;
+
+        transition: background-color 0.3s ease-in-out;
+    }
+
+    .control:hover { background-color: var(--highlight); cursor: pointer; }
+</style>
+  
--- src/lib/numbers/NumberEntry.svelte
+++ src/lib/numbers/NumberEntry.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-    export let control:number|string;
+    import { solver } from "../../stores";
+
+    export let control:number;
 
     function click(e:Event) {
-        if (typeof control == "number") {
-
-        } else {
-            if (control == "note") {
-
-            } else {
-
-            }
-        }
+        
     }
 </script>
 
 <div class="number" on:click="{click}">
-    
+    <div class="inner">{control}</div>
+    <div class="num-left">{9 - ($solver.cBoard.split(`${control}`).length - 1)}</div>
 </div>
 
 <style>
@@ -25,6 +20,31 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
+
+        border-radius: 50px;
+        border: 1px solid var(--highlight);
+
+        width: 35px;
+        height: 35px;
+        padding: 7px;
+
+        margin: 0px 5px;
+
+        position: relative;
+    }
+
+    .number:hover { cursor: pointer; }
+
+    .inner {
+        font-size: 24px;
+        color: var(--font-color);
+    }
+
+    .num-left {
+        position: absolute;
+        font-size: 12px;
+        bottom: 1px;
     }
 </style>
   
